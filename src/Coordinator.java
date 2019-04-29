192a50a9f18d8448137c44bfed086b531436c7ea
msi1 <r.sekaleshfar@gmail.com>
2019-04-30 04:00:04 +0430

    implement logic except the mini games

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,4 +1,36 @@
+import io.ClientHandler;
+import model.GameLogic;
+
 public class Coordinator
 {
+    private GameLogic gameLogic;
+    private ClientHandler clientHandler;
 
+
+    public Coordinator(GameLogic gameLogic, ClientHandler clientHandler) // TODO get UI
+    {
+        this.gameLogic = gameLogic;
+        this.clientHandler = clientHandler;
+    }
+
+    public void coordinateGame()
+    {
+        /*
+        init ui
+         */
+
+        while (true)
+        {
+            /*
+            send to ui
+            send to client
+            start receiving
+            wait
+            stop receiving
+            receive
+            simulate
+            end if finished
+             */
+        }
+    }
 }
--- src/model/GameLogic.java
+++ src/model/GameLogic.java
@@ -1,6 +1,86 @@
 package model;
 
+import message.Direction;
+import message.ReceivedMessage;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class GameLogic
 {
+    private Map map;
+    private HashMap<String, Player> playersMap;
+    private HashMap<String, MiniGame> miniGames;
 
+    public GameLogic()
+    {
+        this.map = new Map(100, 100);
+        this.playersMap = new HashMap<>();
+        this.miniGames = new HashMap<>();
+    }
+
+    public boolean addPlayer(String username)
+    {
+        if (playersMap.containsKey(username))
+        {
+            return false;
+        }
+
+        Cell cell = map.getRandomEmptyCell();
+        Player player = new Player(username, cell);
+        playersMap.put(username, player);
+        return true;
+    }
+
+    public void performMessages(List<ReceivedMessage> receivedMessages)
+    {
+        for (ReceivedMessage receivedMessage : receivedMessages)
+        {
+            String username = receivedMessage.getUsername();
+            Direction direction = receivedMessage.getDirection();
+            boolean isButtonPushed = receivedMessage.isButtonPushed();
+            Player player = playersMap.get(username);
+            movePlayer(player, direction);
+            player.playMiniGame(isButtonPushed, direction);
+        }
+
+        checkCoins();
+        startMiniGames();
+    }
+
+    private void startMiniGames()
+    {
+        // TODO
+    }
+
+    private void checkCoins()
+    {
+        Set<Cell> coinCells = map.getCoinCells();
+        Set<Cell> emptyCells = new HashSet<>();
+
+        for (Cell coinCell : coinCells)
+        {
+            if (!coinCell.hasPlayers())
+            {
+                continue;
+            }
+
+            for (Player player : coinCell.getPlayers())
+            {
+                player.receiveCoin();
+            }
+            coinCell.setHasCoin(false);
+            emptyCells.add(coinCell);
+        }
+
+        coinCells.removeAll(emptyCells);
+    }
+
+    private void movePlayer(Player player, Direction direction)
+    {
+        Cell targetCell = map.getNeighbour(player.getCell(), direction);
+        player.move(targetCell);
+    }
 }
